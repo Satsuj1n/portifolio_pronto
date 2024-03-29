a775139b7953c7994255b91a4ebf82a0dc265352
Felipe Lima <pegasusyt1@gmail.com>
2024-03-29 19:39:34 -0300

    API_KE

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_fqNTaq9i_AkbdxR974XBUPpMzZpMPHj59");
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
